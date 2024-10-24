52caaa65e721c11c053f0e25a3ccbadf84837653
SANSKAR KASHYAP <kashyapsanskar95@gmail.com>
2024-10-24 15:13:37 +0530

    Created Production Page

---
+++ src/Components/ProductSection.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './ProductSection.css'
+
+const ProductSection = () => {
+  return (
+    <div>
+        <section className="product-section">
+            <div className='text-content'>
+               <h1>Your Feet Deserve the Best</h1>
+               <p>Your feet deserve the best, and we’re here to help you with our shoes. Your feet deserve the
+                 best, and we’re here to help you with our shoes.</p>
+               
+            </div>
+        </section>
+      
+    </div>
+  )
+}
+
+export default ProductSection
